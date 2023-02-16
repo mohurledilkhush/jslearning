d55b7f5f6efd001e310e62508bb94f7350a0a305
mohurledilkhush <mohurledilkhush@gmail.com>
2023-02-16 16:36:29 +0530

    js variable

--- hellowWorld.js
+++ hellowWorld.js
@@ -1,1 +1,10 @@
 console.log("dilkhush mohurle");
+console.log("30");
+var fullName="dilkhush mohurle";
+console.log(fullName);
+fullName="kerba"
+console.log(fullName);
+var university;
+console.log(university);
+university="rtmnu";
+console.log("univercity is",university);
---
+++ variableAsign.js
@@ -0,0 +1,24 @@
+console.log("*****step1*****");
+var myFullName;
+myFullName="Dilkhush Mohurle";
+console.log("My name is:",myFullName);
+myFullName="Dilkhush Damodhar Mohurle";
+console.log("My full name is:",myFullName);
+console.log("*****step2*****");
+var myLove="i love only js";
+console.log(myLove);
+console.log("*****step3*****");
+var firstName="Dilkhush"
+console.log("first name:",firstName);
+var lastName="Mohurle"
+console.log("last name:",lastName);
+var age="29"
+console.log("age:",age);
+var address="chandrapur"
+console.log("address:",address);
+var pinCode="441224"
+console.log("pin code:",pinCode);
+var gender="male"
+console.log("gender:",gender);
+var isMarried="No"
+console.log("is married:",isMarried);
